97ad63601a1b02481e8be9e5e8318d92857bf299
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-04-11 16:07:50 +0300

    Create authorization middlewaare

--- routes.php
+++ routes.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 // $router->get('/Workopia/', 'controllers/home.php');
@@ -11,22 +11,19 @@ $router->get('/Workopia/public', 'HomeController@index');
 // All listings page
 $router->get('/Workopia/public/listings', 'ListingController@index');
 // Path to create form
-$router->get('/Workopia/public/create', 'ListingController@create');
+$router->get('/Workopia/public/create', 'ListingController@create', ['auth']);
 //details listing
 $router->get('/Workopia/public/listing', 'ListingController@show');
 // // Path to edit form
-$router->get('/Workopia/public/edit', 'ListingController@edit');
-$router->put('/Workopia/public/edit', 'ListingController@update');
+$router->get('/Workopia/public/edit', 'ListingController@edit', ['auth']);
+$router->put('/Workopia/public/edit', 'ListingController@update', ['auth']);
 //Delete listing 
-$router->delete('/Workopia/public/listing', 'ListingController@destroy');
+$router->delete('/Workopia/public/listing', 'ListingController@destroy', ['auth']);
 // Create listing
-$router->post('/Workopia/public/listings', 'ListingController@store');
+$router->post('/Workopia/public/listings', 'ListingController@store', ['auth']);
 
-$router->get('/Workopia/public/login', 'UserController@login');
-$router->get('/Workopia/public/register', 'UserController@create');
-$router->post('/Workopia/public/register', 'UserController@store');
-$router->post('/Workopia/public/logout', 'UserController@logout');
-$router->post('/Workopia/public/login', 'UserController@auth');
-
-
-
+$router->get('/Workopia/public/login', 'UserController@login', ['quest']);
+$router->get('/Workopia/public/register', 'UserController@create', ['quest']);
+$router->post('/Workopia/public/register', 'UserController@store', ['quest']);
+$router->post('/Workopia/public/logout', 'UserController@logout', ['auth']);
+$router->post('/Workopia/public/login', 'UserController@auth', ['quest']);
